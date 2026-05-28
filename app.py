7f9b9cc9fa3d4fd715b8d957e3d5763c5d15f2bd
lyd1amc <lydiafernandinha@gmail.com>
2026-05-28 08:35:34 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
 
-cardapio_lanches = {
-    1: {"nome": "X-Burguer", "preco": 15.00, "ingredientes": "Pão, carne de 120g e queijo prato."},
-    2: {"nome": "X-Salada", "preco": 18.00, "ingredientes": "Pão, carne, queijo, alface, tomate e maionese."},
-    3: {"nome": "X-Bacon", "preco": 22.00, "ingredientes": "Pão, carne, queijo, bacon crocante e molho barbecue."}
-}
+app.secret_key = 'sua_chave_secreta'
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        nome = request.form.get('nome', '').strip()
+        email = request.form.get('email', '').strip().lower().lower
 
-@app.route('/cardapio')
-def mostrar_cardapio():
-    return render_template('cardapio.html', produtos=cardapio_lanches)
-
-@app.route('/lanche/<int:id>')
-def detalhe_lanche(id):
-    lanche_selecionado = cardapio_lanches.get(id)
-    return render_template('lanche.html', lanche=lanche_selecionado)
-
-@app.route('/contato')
-def contato():
-    return render_template('contato.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+       if not name:
+        flash('O nome é obrigatório.', 'erro')   
+        return redirect(url_for('cadastro'))
+    
+       if not email:
+        flash('O e-mail é obrigatório.', 'erro')   
+        return redirect(url_for('cadastro'))
